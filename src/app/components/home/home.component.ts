a3e61c463b886675fffdd17fd509ba8854a99b7b
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-08-12 09:30:45 -0400

    consumo apis y optimizaciones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { trigger, transition, query, stagger, animate, style } from '@angular/animations';
+import { take } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+
+// Services and subscriptions
+import { MovieService } from '../../services/movie.service';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
-})
-export class HomeComponent implements OnInit {
+  styleUrls: ['./home.component.sass'],
+  animations: [
 
-  constructor() { }
+  ]
+
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  // Declarations
+  public viewCount = 9;
+  public page = 0;
+  public pageSize = 9;
+  public previousPage = 0;
+  public movies = [];
+  public moviesStorage = [];
+  public objectMovie: any;
+  public subscriptionSearchData: Subscription;
+  private componentDestroyed: Subject<boolean> = new Subject();
+  public message = null;
+
+  constructor(
+    private movieService: MovieService,
+    private router: ActivatedRoute
+  ) {
+      this.subscriptionSearchData = this.movieService.observableSearchData$
+        .subscribe(
+          dataSearch => {
+            if(dataSearch) {
+              this.getDataSearch(dataSearch);
+            }
+          });
+   }
 
   ngOnInit() {
+    this.router.params.subscribe(routeParams => {
+      this.getPopular(routeParams.category);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed.next(true);
+    this.componentDestroyed.complete();
+    this.subscriptionSearchData.unsubscribe();
+  }
+
+  public changeViewMovie() {
+    this.movies = [];
+    this.movies = this.moviesStorage.slice(0, this.viewCount);
+  }
+
+  public getPopular(category: string) {
+    
   }
 
 }
--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -5,7 +5,33 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 
+@Injectable()
 export class JwtInterceptor implements HttpInterceptor {
     // Interceptar y manejar una solicitud http dada
-    
+    constructor(
+        private router: Router) {}
+
+    // function intercept
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTQ4ODgzZTAxYWE5NjNjZThkOGE2ZDkwMDVkYjE1MSIsInN1YiI6IjVkNWQ0ZjQ4YTNkMDI3MDAxNDBiZDkzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w4iJG7DNBvihwHWQI4wiCPmuE-7vao2FBV9hnNRtFxg';
+        req = req.clone({
+            setHeaders: {
+                Authorization: `${token}`
+            }
+        });
+
+        return next.handle(req).pipe(
+            tap((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    //console.log(event);
+                }
+            }, (err: any) => {
+                if(err instanceof HttpErrorResponse) {
+                    if(err.status === 401) {
+                        this.router.navigate(['/auth']);
+                    }
+                }
+            })
+        );
+    }
 }
